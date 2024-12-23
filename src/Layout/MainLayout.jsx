026f9de690afaeee7cd651a2103291f4b66f0465
AsifurRahman10 <1720105@iub.edu.bd>
2024-12-23 15:40:00 +0600

    navbar added

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Navbar } from "../Component/Navbar";
+
+export const MainLayout = () => {
+  return (
+    <div className="bg-[#FFFFFF]">
+      <Navbar></Navbar>
+    </div>
+  );
+};
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custom-btn': '#97643f', //theme color
+      },
+    },
+  },
+  plugins: [
+    require('daisyui'),
+    require("flowbite/plugin"),
+  ],
+}
